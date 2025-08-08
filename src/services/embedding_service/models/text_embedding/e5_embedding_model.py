cbba9d6f27fd3a44fd66b63bb90968da61316e7f
KiRzEa <kietnt0603@gmail.com>
2025-08-08 17:37:03 +0700

    feat: add kwargs for embedding model

--- src/services/embedding_service/models/text_embedding/e5_embedding_model.py
+++ src/services/embedding_service/models/text_embedding/e5_embedding_model.py
@@ -1,4 +1,5 @@
-from typing import Union, List
+import torch
+from typing import Union, List, Dict
 
 from langchain_huggingface import HuggingFaceEmbeddings
 
@@ -7,17 +8,27 @@ from src.services.embedding_service.models.text_embedding.base import BaseTextEm
 class E5TextEmbeddingModel(BaseTextEmbeddingModel):
     def __init__(
             self,
-            model_name: str = "tranguyen/halong_embedding-legal-document-finetune"
+            model_name: str = "tranguyen/halong_embedding-legal-document-finetune",
+            model_kwargs: Dict = {"device": "cuda" if torch.cuda.is_available() else "cpu"},
+            encode_kwargs: Dict = {"normalize_embeddings": False}
     ):
-        self.model = self.load_model(model_name)
+        self.model_name = model_name
+        self.model_kwargs = model_kwargs
+        self.encode_kwargs = encode_kwargs
+
+        self.load_model()
 
     @property
     def embed_dim(self) -> int:
         return 768
     
-    def load_model(self, model_name: str) -> HuggingFaceEmbeddings:
-        model = HuggingFaceEmbeddings(model_name=model_name)
-        return model
+    def load_model(self) -> HuggingFaceEmbeddings:
+        self.model = HuggingFaceEmbeddings(
+            model_name=self.model_name,
+            model_kwargs=self.model_kwargs,
+            encode_kwargs=self.encode_kwargs,
+        )
+
     def preprocess(self, batch):
         pass
 
